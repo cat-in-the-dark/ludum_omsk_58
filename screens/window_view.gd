51a20559414d69a56a65878e45b93787227866a9
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-03 23:13:10 +0600

    feat: setup windowed version

--- screens/window_view.gd
+++ screens/window_view.gd
@@ -2,4 +2,9 @@ extends Window
 
 func _ready() -> void:
 	transient = true # Make the window considered as a child of the main window
-	close_requested.connect(queue_free) # Actually close the window when clicking the close button
+	close_requested.connect(on_close) # connect window closing event
+	
+func on_close() -> void:
+	# queue_free() # close this window
+	# quit the game on any window is closed
+	get_tree().quit()
