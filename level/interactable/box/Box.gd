349f5bb4e4c954f2a21f74ca118b15fb9963853b
Dmitry Bagaev <devilprime5@gmail.com>
2025-10-05 02:29:52 +0600

    feat: added box grabbing; remove run command; added interact command

--- level/interactable/box/Box.gd
+++ level/interactable/box/Box.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 @onready var body = $"."
+@onready var model = $Model
 var body_mass = 10
 @onready var box_start_position = global_position
 
@@ -9,11 +10,16 @@ func _ready() -> void:
 	Events.kill_plane_touched.connect(func on_kill_plane_touched() -> void:
 		global_position = box_start_position
 		linear_velocity = Vector3.ZERO
+		angular_velocity = Vector3.ZERO
 		set_physics_process(true)
 	)
 	Events.kill_plane_touched_with_id.connect(func on_kill_plane_touched_with_id(killedObjectId: int) -> void:
 		if killedObjectId == get_instance_id():
 			global_position = box_start_position
 			linear_velocity = Vector3.ZERO
+			angular_velocity = Vector3.ZERO
 			set_physics_process(true)
 	)
+
+func getMockObject():
+	return model.duplicate()
