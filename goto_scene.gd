4af592bffb4b310fb255c596aeeaad10ab173c0b
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-04 18:49:11 +0600

    game win screen and loop

--- goto_scene.gd
+++ goto_scene.gd
@@ -6,4 +6,4 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select"):
 		get_tree().change_scene_to_file(next_scene)
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://menu_scene_split_windows.tscn")
+		get_tree().change_scene_to_file(next_scene)
--- level/interactable/flag/flag_reached_screen.gd
+++ level/interactable/flag/flag_reached_screen.gd
@@ -1,12 +1,13 @@
 extends CanvasLayer
 
 @onready var _animation_player: AnimationPlayer = %AnimationPlayer
-
+@export_file_path var next_scene: String
 
 func _ready() -> void:
 	Events.flag_reached.connect(func on_flag_reached() -> void:
 		await get_tree().create_timer(2.0).timeout
 		_animation_player.play("fade_in")
 		await _animation_player.animation_finished
-		get_tree().quit()
+		if next_scene != null and next_scene != "":
+			get_tree().change_scene_to_file(next_scene)
 	)
