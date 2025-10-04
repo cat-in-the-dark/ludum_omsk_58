4ba6dc0cdf120852bf6eb061244502c1bf7535b4
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-04 19:32:26 +0600

    fix bug in goto scene

--- goto_scene.gd
+++ goto_scene.gd
@@ -4,6 +4,8 @@ extends Node
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select"):
+		print("Goto: ", next_scene)
 		get_tree().change_scene_to_file(next_scene)
-	if Input.is_action_just_pressed("ui_accept"):
+	elif Input.is_action_just_pressed("ui_accept"):
+		print("Goto: ", next_scene)
 		get_tree().change_scene_to_file(next_scene)
