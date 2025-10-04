89c524f49e38aef9a1c47d3e7d97b072ac679fb1
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-04 15:54:42 +0600

    allow to start in split screen

--- menu.gd
+++ menu.gd
@@ -1,5 +1,9 @@
 extends Node
 
+@export_file_path var next_scene: String
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select"):
-		get_tree().change_scene_to_file("res://main_split_windows.tscn")
+		get_tree().change_scene_to_file(next_scene)
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://menu_scene_split_windows.tscn")
