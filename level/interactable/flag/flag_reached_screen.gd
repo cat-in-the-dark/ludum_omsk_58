c7cad1302c3522b7930978e1c2efd6ccaa0ed731
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-06 19:21:02 +0600

    refactor project structure

--- level/interactable/flag/flag_reached_screen.gd
+++ level/interactable/flag/flag_reached_screen.gd
@@ -9,8 +9,8 @@ func _ready() -> void:
 	startMSec = Time.get_ticks_msec()
 	Events.flag_reached.connect(func on_flag_reached() -> void:
 		await get_tree().create_timer(2.0).timeout
-		var endMSec: int = Time.get_ticks_msec()
-		Globals.level_cleared_in_seconds = (endMSec - startMSec) / 1000
+		var endMSec := Time.get_ticks_msec()
+		Globals.level_cleared_in_seconds = roundi((endMSec - startMSec) / 1000.0)
 		_animation_player.play("fade_in")
 		await _animation_player.animation_finished
 		if next_scene != null and next_scene != "":
