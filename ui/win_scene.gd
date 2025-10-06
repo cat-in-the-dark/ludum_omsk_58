87e81faad7234994c4793c4b5aa4f5e07aa03d54
Ilya Siganov <ilya.blan4@gmail.com>
2025-10-06 22:36:20 +0600

    fix gamewin, add icon and name

--- ui/win_scene.gd
+++ ui/win_scene.gd
@@ -3,5 +3,5 @@ extends Node
 @export_file_path var next_scene: String
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_start_1") or Input.is_action_just_pressed("ui_start_2"):
+	if Input.is_action_just_pressed("ui_restart"):
 		get_tree().change_scene_to_file(next_scene)
